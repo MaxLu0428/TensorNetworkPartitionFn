0598c15ceecc9795fd6b227956f2ecdad26fcd5e
MaxLu <maxlu0428@gmail.com>
2020-11-27 14:23:07 +0800

    To do: add function to get eigenvalue of transfer matrix

--- Transfer_matrix_main.py
+++ Transfer_matrix_main.py
@@ -20,14 +20,14 @@ import os
 
 
 
-  
-    
+
+
 def SAVE_dATA( Cdata,RG_step,a_list,Dcut,name):
-    
+
     dataframe = pd.DataFrame( Cdata )
     dataframe.index=['L={}'.format(2**(num_steps+1)) for num_steps in range(RG_step) ]
-    dataframe.columns = [ '%.3f'%a1  for a1 in a_list  ] 
-    
+    dataframe.columns = [ '%.4f'%a1  for a1 in a_list  ]
+
     dataframe = dataframe.stack()
     dataframe = dataframe.unstack(0)
     dataframe.index.name="Dcut="+str(Dcut)
@@ -41,90 +41,68 @@ def transfer_matrix(T):
     TM = np.reshape(T4,(dc*dc, dc*dc) )
     return TM
 
+def transfer_matrix2(T):
+    ''' instead of 2x2 unitcell , try to use 2x1 '''
+    T2 = np.tensordot(T,T,axes=([1,3],[3,1]))
+    dc = T2.shape[0]
+    TM = np.reshape(T2,(dc*dc,dc*dc))
+    return TM
+
+def transfer_matrix3(T):
+    dims = T.shape
+    result = np.zeros((dims[0],dims[2]))
+
+    for i in range(dims[0]):
+        for j in range(dims[2]):
+            result[i][j] = np.trace(T[i,:,j,:])
+    return result
 
 #================================================#
-trgstep = 2
+trgstep = 15
 N = 2
 step= 40
 deltT = (4.0/step)
 dcut = 16
-
-Tc = 2.0/np.log(1.0 + np.sqrt(2)) 
+FILE = './transfer_matrix'
+Tc = 2.0/np.log(1.0 + np.sqrt(2))
 
 
 
 #FD_D = dict()
-DT, IDT = HOTRG_two.Ising_exact(2,1/Tc,'float')
-
-T0 = DT;  iT0 = IDT; N_list = []; ti=0; FE_list = np.zeros(trgstep)
-N1 = np.max(abs(T0))
-T0 = T0/N1
-N_list.append(N1)
-
-TM = transfer_matrix( T0 )
-Y, Z = linalg.eigh( TM)
-print (Y)
 
 
-for RG in range(trgstep):
-    
-    T1,UU,UUT,N1 = HOTRG_two.updat_pure( T0,T0,'y',dcut)
-    iT1 = HOTRG_two.updat_impurity( iT0, T0,'y',dcut,UU,UUT,N1)
+
+
+T_list  = [Tc]
+correlation_length = dict()
+
+for T in T_list:
+    CL_list = np.zeros(trgstep)
+    DT, IDT = HOTRG_two.Ising_square(T,bias=10**-5)
+    T0 = DT;  iT0 = IDT; N_list = []; ti=0; FE_list = np.zeros(trgstep)
+    N1 = np.max(abs(T0))
+    T0 = T0/N1
     N_list.append(N1)
-            
-        ## update along x-direction
-    T2,UU,UUT,N1 = HOTRG_two.updat_pure( T1,T1,'x',dcut)
-    iT2 = HOTRG_two.updat_impurity( iT1, T1,'x',dcut,UU,UUT,N1)     
-    N_list.append(N1)
-        
-    T0 = T2;  iT0 = iT2
-    
     TM = transfer_matrix( T0 )
     Y, Z = linalg.eigh( TM)
-    
-    print (Y)
-    
-    
-    
-#directory = './results_L%s_T%s' % (L,'%.3f'%T)
-#directory = './Ising'  
-#os.makedirs(directory)
-#filename = directory +'/FE_D' + str(dcut) + '.csv'
-#SAVE_dATA( FE_D, trgstep,T_list,dcut,filename)
-    
-        
-    
-    
-    
-    
-    
-    
+    for RG in range(trgstep):
 
+        T1,UU,UUT,N1 = HOTRG_two.updat_pure( T0,T0,'y',dcut)
+        iT1 = HOTRG_two.updat_impurity( iT0, T0,'y',dcut,UU,UUT,N1)
+        N_list.append(N1)
+        ## update along x-direction
+        T2,UU,UUT,N1 = HOTRG_two.updat_pure( T1,T1,'x',dcut)
+        iT2 = HOTRG_two.updat_impurity( iT1, T1,'x',dcut,UU,UUT,N1)
+        N_list.append(N1)
+        T0 = T2;  iT0 = iT2
+        TM = transfer_matrix3( T0 )
+        Y, Z = linalg.eigh( TM)
+        print("L={}".format(2**RG*2))
 
-#
-#    
-#    
-#clist=['r','k','g','b','y','lightsalmon','silver','lime','steelblue','olive']
-#makerlist =['o','x','+','D','s','^','<','>','o','x']
-#    
-#f = plt.figure(figsize=(8, 6)); # plot the calculated values    
-#ax = plt.axes([.15, .12, .7, .78])
-#
-#plt.plot(T_list, MZlist,c = clist [0], ls='--',lw= 1.0,    ##label='$\mathcal{O} (n!)$',
-#         marker = makerlist[0], mec= clist[0], mfc= 'none', mew = 1.2,  ms = 7)  
-#
-#
-#
-#plt.xlabel("$n$", fontsize=20);
-##plt.ylabel(" ", fontsize=20);       
-#plt.axis('tight')
-#plt.legend( loc=1, fontsize=14)
-##plt.xlim(0,val)
-##plt.ylim(0,5000)
-##plt.title('$HOTRG \chi$='+str(chi_max), fontsize= 20)  
-##plt.text(0.1,0.1, 'Lowest : -0.6167', transform=ax.transAxes, ha='left', fontsize=15)
-#   
-#plt.show()
-#
-#
-#
+        lambda1 = Y[-1]
+        lambda2 = Y[-2]
+        print('correlation length ')
+        print(1/np.log((lambda1/lambda2)))
+        CL_list[RG] = 1/np.log(np.abs(lambda1/lambda2))
+    correlation_length[T] = CL_list;
+# SAVE_dATA(correlation_length, trgstep,T_list,dcut,FILE+'/_D'+str(dcut)+'.csv' )
